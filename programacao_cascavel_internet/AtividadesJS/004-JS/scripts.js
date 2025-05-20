b9a8f35df34bd4562eee08d2d6bf83a7953e1c41
Gabrielaraujo0102 <gabriel10207@edu.unipar.br>
2025-05-19 22:06:09 -0300

    Add files via upload

--- programacao_cascavel_internet/AtividadesJS/004-JS/scripts.js
+++ programacao_cascavel_internet/AtividadesJS/004-JS/scripts.js
@@ -1,13 +1,3 @@
-function reajustarSaldo() {
-    const saldoInput = document.getElementById('saldo');
-    const saldo = parseFloat(saldoInput.value);
-  
-    if (isNaN(saldo)) {
-      document.getElementById('resultado').innerText = "Por favor, insira um valor válido.";
-      return;
-    }
-  
-    const saldoReajustado = saldo * 1.01;
-    document.getElementById('resultado').innerText =
-      `Saldo com reajuste de 1%: R$ ${saldoReajustado.toFixed(2)}`;
-  }
+  let inputNum3 = document.querySelector("#");
+  let h3Resultado = document.querySelector("#");
+  let  = document.querySelector("#");
